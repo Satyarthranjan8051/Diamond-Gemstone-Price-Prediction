dca4b1f31f86fe7eed53f3064442c9c442e5efe0
Satyarthranjan8051 <SatyarthRanjan4488@gmail.com>
2025-04-18 22:21:21 +0530

    change setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ from typing import List
 
 HYPEN_E_DOT='-e .'
 
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+# def get_requirements(file_path:str)->List[str]:
+#     requirements=[]
+#     with open(file_path) as file_obj:
+#         requirements=file_obj.readlines()
+#         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
 
-    return requirements
+#     return requirements
 
 setup(
     name="DimondPricePrediction",
     version="0.0.1",
     author="Satyarth Ranjan",
     author_email="satyarthranjan4488@gmail.com",
+    install_requires=["pandas","numpy", "scikit-learn"],
     packages=find_packages()
 )
