0f4ff32ad52ef2f720b6c5caa15579d5d4d9f487
Satyarthranjan8051 <SatyarthRanjan4488@gmail.com>
2025-04-18 20:57:53 +0530

    try to install local package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="DimondPricePrediction",
+    version="0.0.1",
+    author="Satyarth Ranjan",
+    author_email="satyarthranjan4488@gmail.com",
+    packages=find_packages()
+)
